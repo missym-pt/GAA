215a4edaa252349c06ebeec927968e07668a0b7c
UserPL135Pc11 <UserPL135Pc11@SLFRPL135PC11.ensino.atec.local>
2016-10-26 10:41:21 +0100

    Introdução

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mensagem = "Olá turma";
+		System.out.println(mensagem);
+
+	}
+
+}
